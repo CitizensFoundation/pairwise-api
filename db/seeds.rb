18d18690f5b8956df9c10255cee35d50fd11708e
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-10-24 14:03:49 +0000

    Work on deploymnet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # rake db:seed
 # load the API user for AOI dev environment
 
-u = User.new(:email => "pairwisetest@dkapadia.com", :password => "wheatthins")
+u = User.new(:email => ENV['PAIRWISE_API_USER'], :password => ENV["PAIRWISE_API_PASSWORD"])
 u.save(:validate => false)
