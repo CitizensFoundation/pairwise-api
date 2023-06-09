5436daeb2064c28149de54f63e1fb33518fa9745
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-06-09 18:46:15 +0000

    Work on redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,11 @@
 require 'redis'
-$redis = Redis.new(:host => REDIS_CONFIG['hostname'])
+require 'yaml'
+
+redis_config = YAML.load_file(Rails.root.join('config', 'redis.yml'))[Rails.env]
+
+$redis = Redis.new(
+  host: redis_config['host'],
+  port: redis_config['port'],
+  username: redis_config['username'],
+  password: redis_config['password']
+)
