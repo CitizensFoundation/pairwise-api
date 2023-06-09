1fc265277845ee9cf63c18411fc5c87907efe46a
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-06-09 19:03:17 +0000

    Work

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,11 @@
 require 'redis'
 
+puts "REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS REDIS"
+puts "Host: #{ENV['REDIS_HOST']}"
+puts "Port: #{ENV['REDIS_PORT']}"
+puts "Username: #{ENV['REDIS_USERNAME']}"
+puts "Password: #{ENV['REDIS_PASSWORD']}"
+
 $redis = Redis.new(
   host: ENV['REDIS_HOST'],
   port: ENV['REDIS_PORT'].to_i,
